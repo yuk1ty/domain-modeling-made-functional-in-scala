87d3c2612b7b6574d999d283761dc65ee007dd35
yuk1ty <yuk1ty@users.noreply.github.com>
2024-12-25 00:20:26 +0900

    Chapter5

--- src/main/scala/com/github/yuk1ty/domainModelingMadeFunctional/common/SimpleTypes.scala
+++ src/main/scala/com/github/yuk1ty/domainModelingMadeFunctional/common/SimpleTypes.scala
@@ -23,3 +23,53 @@ enum ProductCode {
   case Widget(code: WidgetCode)
   case Gizmo(code: GizmoCode)
 }
+
+case class UnitQuantity(value: Int) extends AnyVal
+
+case class KilogramQuantity(value: BigDecimal) extends AnyVal
+
+enum OrderQuantity {
+  case Unit(quantity: UnitQuantity)
+  case Kilogram(quantity: KilogramQuantity)
+}
+
+case class CustomerId(value: String) extends AnyVal
+
+case class CustomerInfo(value: String) extends AnyVal
+
+case class ShippingAddress(value: String) extends AnyVal
+
+case class BillingAddress(value: BigDecimal) extends AnyVal
+
+case class Price(value: BigDecimal) extends AnyVal
+
+case class Order(
+    id: OrderId,
+    customerId: CustomerId,
+    shippingAddress: ShippingAddress,
+    billingAddress: BillingAddress,
+    orderLines: List[Order.OrderLine]
+)
+
+object Order {
+  case class OrderLine(
+      id: OrderLineId,
+      orderId: OrderId,
+      productCode: ProductCode,
+      orderQuantity: OrderQuantity,
+      price: Price
+  )
+}
+
+enum PlaceOrderError {
+  case Validation(
+      val validationError: List[PlaceOrderError.ValidationError]
+  )
+}
+
+object PlaceOrderError {
+  case class ValidationError(
+      val fieldName: String,
+      val errorDescription: String
+  )
+}
